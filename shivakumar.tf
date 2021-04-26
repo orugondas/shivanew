8bfb5cf74ee8f408f080aed947e385edfaafbcb0
orugondas <83070490+orugondas@users.noreply.github.com>
2021-04-26 17:29:20 +0530

    Update shivakumar.tf

--- shivakumar.tf
+++ shivakumar.tf
@@ -7,7 +7,7 @@ resource "aws_elasticache_cluster" "shivakumar" {
 cluster_id = "shivakumars"
 engine = "redis"
 node_type = "cache.t2.micro"
-num_cache_node = "1"
+num_cache_node = 1
 parameter_group_name = "defualt.redis3.2"
 engine_version = "3.2.10"
 port = "6379"
