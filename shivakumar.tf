d98ceded82457c0cf983d5150ea060580c2295c0
orugondas <83070490+orugondas@users.noreply.github.com>
2021-04-26 17:32:19 +0530

    Update shivakumar.tf

--- shivakumar.tf
+++ shivakumar.tf
@@ -3,12 +3,7 @@ region = "us-east-2"
 access_key = "AKIAX4EFVBORSRYNT5A7"
 secret_key = "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"
 }
-resource "aws_elasticache_cluster" "shivakumar" {
-cluster_id = "shivakumars"
-engine = "redis"
-node_type = "cache.t2.micro"
-num_cache_node = 1
-parameter_group_name = "defualt.redis3.2"
-engine_version = "3.2.10"
-port = "6379"
-}
+resource "aws_s3_bucket" "shivakumar123" {
+bucket = "shiva147789"
+acl = "private"
+  }
