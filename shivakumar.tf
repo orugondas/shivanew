44aac0c477e0f413f9abc4b2221df9e606c387c3
orugondas <83070490+orugondas@users.noreply.github.com>
2021-04-26 16:59:08 +0530

    Update shivakumar.tf

--- shivakumar.tf
+++ shivakumar.tf
@@ -1,12 +1,7 @@
 provider "aws" {
 region = "us-east-2"
-access_key = "AKIAX4EFVBORXZZDY6OE"
-secret_key = "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"
-}
-resource "aws_instance" "shivakumar"{
-ami = "ami-05d72852800cbf29e"
-instance_type = "t2.micro"
-key_name = "123456"
+access_key = "AKIAX4EFVBORSRYNT5A7"
+secret_key = "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"
 }
 resource "aws_elasticache_cluster" "shivakumar" {
 cluster_id = "shivakumars"
