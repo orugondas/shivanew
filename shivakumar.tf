83e5a30d66e8363592b569bed3b0a158f993fa47
orugondas <83070490+orugondas@users.noreply.github.com>
2021-05-10 23:49:27 +0530

    Update shivakumar.tf

--- shivakumar.tf
+++ shivakumar.tf
@@ -1,7 +1,7 @@
 provider "aws" {
 region = "us-east-2"
-access_key = "AKIAX4EFVBORSRYNT5A7"
-secret_key = "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"
+access_key = "AKIAX4EFVBOR4OUQMOEF"
+secret_key = "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"
 }
 resource "aws_s3_bucket" "shivakumar123" {
 bucket = "shiva147789"
